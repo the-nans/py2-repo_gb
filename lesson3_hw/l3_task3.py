7bb72b29f664647cf841b6502c24d07f8bccb03a
the-Nans <agata.a.bobyleva@gmail.com>
2021-01-27 10:10:06 +0300

    fix task3

--- lesson3_hw/l3_task3.py
+++ lesson3_hw/l3_task3.py
@@ -3,12 +3,12 @@
 """
 from random import randint
 arr_spread = 100
-arr_size = 20
+arr_size = 4
 arr = [randint(1, arr_spread) for _ in range(arr_size)]
 print(arr)
-m, k = 0, arr[0]
-for i in arr:
-    m = i if m < i else m
-    k = i if k > i else k
-arr[arr.index(m)], arr[arr.index(k)] = arr[arr.index(k)], arr[arr.index(m)]
+m, k = 0, 0
+for i in range(len(arr)):
+    m = i if m < arr[i] else m
+    k = i if k > arr[i] else k
+arr[m], arr[k] = arr[k], arr[m]
 print(arr)
--- lesson3_hw/l3_task9.py
+++ lesson3_hw/l3_task9.py
@@ -4,7 +4,7 @@
 from random import randint # для ленивый
 cols = 5
 rows = 5
-arr_spread = 10
+arr_spread = 100
 spam, res = [], []
 ex = arr_spread
 matrix = [[randint(1, arr_spread) for i in range(rows)] for k in range(cols)]
@@ -12,16 +12,28 @@ for line in matrix:
     for item in line:
         print(f"{item:<4}", end='')
     print()
+# =================================
+# for i in range(cols):
+#     for line in matrix:
+#         spam.append(line[i])
+#     for j in spam:
+#         ex = j if j < ex else ex
+#     res.append(ex)
+#     spam.clear()
+#     ex = arr_spread
+# ex = 0
+# print(f'Column minimums are {res}')
+# for i in res:
+#     ex = i if i > ex else ex
+# print(f'Max element of column minimums is {ex}')
+
+# ==================================
+max_ = 0
 for i in range(cols):
-    for line in matrix:
-        spam.append(line[i])
-    for j in spam:
-        ex = j if j < ex else ex
-    res.append(ex)
-    spam.clear()
-    ex = arr_spread
-ex = 0
-print(f'Column minimums are {res}')
-for i in res:
-    ex = i if i > ex else ex
-print(f'Max element of column minimums is {ex}')
+    min_ = matrix[0][i]
+    for j in range(rows):
+        if matrix[j][i] < min_:
+            min_ = matrix[j][i]
+    if max_ < min_:
+        max_ = min_
+print(max_)
