0801fc222e361681a7734408bd8291bff92028ef
the-Nans <agata.a.bobyleva@gmail.com>
2021-02-10 18:55:20 +0300

    lesson2_sandbox updated

--- lesson2_sandbox/sandbox.py
+++ lesson2_sandbox/sandbox.py
@@ -1,4 +1,5 @@
 from sys import setrecursionlimit
+
 setrecursionlimit(3000)
 
 
@@ -10,15 +11,16 @@ def atob(a, b):
     if a < b:
         return f"{a}, {atob(a + 1, b)}"
 
+
 # глубина рекурсси в Питоне - 1000
 
 
 def akk(m, n):
     if m == 0:
-        return n+1
+        return n + 1
     elif m > 0 and n == 0:
-        return akk(m-1, 1)
-    return akk(m-1, akk(m, n-1))
+        return akk(m - 1, 1)
+    return akk(m - 1, akk(m, n - 1))
 
 
 def nod(p, q):
@@ -45,11 +47,11 @@ def nod3(n, m):
 
 
 def eratosphen(n: int):
-    numbers = list(range(n+1))
+    numbers = list(range(n + 1))
     numbers[1] = 0
     for i in numbers:
         if numbers[i] != 0:
-            j = i*2
+            j = i * 2
             while j < n:
                 numbers[j] = 0
                 j += i
@@ -64,8 +66,8 @@ def dectobin(n):
         sett.append(i)
         i *= 2
     presum = 0
-    for j in range(len(sett)-1, -1, -1):
-        if presum+sett[j] <= n:
+    for j in range(len(sett) - 1, -1, -1):
+        if presum + sett[j] <= n:
             presum += sett[j]
             sett[j] = 1
         else:
@@ -75,22 +77,22 @@ def dectobin(n):
         result = f'{j}{result}'
     return result
 
+
 def likes(names):
     if len(names) == 1:
         return f"{names[0]} likes this"
-    elif len(names) == 2 or len(names)==3:
+    elif len(names) == 2 or len(names) == 3:
         names[-1] = f"and {names[-1]} like this"
         for i in names:
             result = f"{result}, {i}"
-    elif len(names)>3:
-        result = f"{names[0]}, {names[1]} and {len(names)-2} others like this"
+    elif len(names) > 3:
+        result = f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
     elif len(names) == 0:
         return "no one likes this"
     return result
 
 
-
-def dnatranslate(dna : str, frames = [1,2,3,-1,-2,-3]):
+def dnatranslate(dna: str, frames=[1, 2, 3, -1, -2, -3]):
     codons = {'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L', 'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
               'ATT': 'I', 'ATC': 'I', 'ATA': 'I', 'ATG': 'M', 'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
               'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S', 'AGT': 'S', 'AGC': 'S', 'CCT': 'P', 'CCC': 'P',
@@ -111,6 +113,7 @@ def dnatranslate(dna : str, frames = [1,2,3,-1,-2,-3]):
             result = f"{mapper[dna_to_map[count]]}{result}"
             count += 1
         return result
+
     frames_map = {1: 0, 2: 1, 3: 2, -1: 0, -2: 1, -3: 2}
     resres = []
     res = ""
@@ -132,4 +135,118 @@ def dnatranslate(dna : str, frames = [1,2,3,-1,-2,-3]):
 # print(dnatranslate('AAA', [1]))
 # print(dnatranslate('AA', [2]))
 # print(dnatranslate('AAA', [-1]))
-print(dnatranslate("",[]))
+# print(dnatranslate("",[]))
+
+import random, cProfile
+
+
+def bubble(array):
+    j = 1
+    while j < len(array):
+        i = 0
+        while i < len(array) - j:
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+            i += 1
+        j += 1
+
+
+def selection_sort(array):
+    for i in range(len(array)):
+        idx_min = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[idx_min]:
+                idx_min = j
+        array[idx_min], array[i] = array[i], array[idx_min]
+
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        spam = array[i]
+        j = i
+        while array[j - 1] > spam and j > 0:
+            array[j] = array[j - 1]
+            j -= 1
+        array[j] = spam
+
+
+def shell_sort(array=[]):
+    """
+    1, 4, 10, 23, 57, 132, 301, 701, 1750
+    :param array:
+    :return:
+    """
+    assert len(array) < 4000, 'надо бы меньше 4000'
+
+    def new_increment(array):
+        inc = [1, 4, 10, 23, 57, 132, 301, 701, 1750]
+
+        while len(array) <= inc[-1]:
+            inc.pop()
+
+        while len(inc) > 0:
+            yield inc.pop()
+
+    for increment in new_increment(array):
+        for i in range(increment, len(array)):
+            for j in range(i, increment - 1, -increment):
+                print('i=', i, 'j=', j, 'increment =', increment)
+                if array[j - increment] <= array[j]:
+                    break
+                array[j], array[j - increment] = array[j - increment], array[j]
+                print(array)
+
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = random.choice(array)
+    s_ar = []
+    m_ar = []
+    eq_ar = []
+
+    for i in array:
+        if i < pivot:
+            s_ar.append(i)
+        elif i > pivot:
+            m_ar.append(i)
+        else:
+            eq_ar.append(i)
+    print(s_ar, eq_ar, m_ar)
+    return quick_sort(s_ar) + eq_ar + quick_sort(m_ar)
+
+
+def quick_sort_no_memory(array, fst, lst):
+    if fst >= lst:
+        return
+    pivot = array[random.randint(fst,lst)]
+    i, j = fst, lst
+
+    while i <= j:
+
+        while array[i] < pivot:
+            i += 1
+        while array[j] > pivot:
+            j -= 1
+        if i <= j:
+            array[i], array[j] = array[j], array[i]
+            i, j = i+1, j-1
+    quick_sort_no_memory(array, fst, j)
+    quick_sort_no_memory(array, i, lst)
+
+def revers(array=[]):
+    for i in range(len(array)//2):
+        array[i], array[len(array)-i-1] = array[len(array)-i-1], array[i]
+
+from collections import namedtuple
+from operator import attrgetter
+
+Person = namedtuple('Person', ['name', 'age'])
+p_1 = Person("vasya", 25)
+p_2 = Person("kolya", 30)
+p_3 = Person("olya", 33)
+
+people = (p_1, p_2, p_3)
+
+print(sorted(people, key=attrgetter('age')))
